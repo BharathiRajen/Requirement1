bf1109e9dbff5c205f3fd883d3e71e0a48f421ca
BharathiRajendran <84081813+BharathiRajenran@users.noreply.github.com>
2021-12-24 17:10:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requirement1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*User user01 = new User();
+            User user02 = new User();*/
+
+            User[] users = new User[2];
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter User {0} Details:", i + 1);
+                string userdetails = Console.ReadLine();
+                string[] users_details = userdetails.Split(',');
+                users[i] = new User(users_details[0], users_details[1], users_details[2]);
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("User {0}", i + 1);
+                Console.WriteLine(users[i]);
+            }
+            if (users[0].Equals(users[1]))
+            {
+                Console.WriteLine("User 1 is same as User 2");
+            }
+            else
+            {
+                Console.WriteLine("User 1 is Different as User 2");
+            }
+
+
+
+        }
+
+    }
+}
+
+
---
+++ User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requirement1
+{
+    class User
+    {
+        //members
+        private string Username;
+        private string MailId;
+        private string Password;
+
+        //Properties
+
+        public string Username1 { get => this.Username; set => this.Username = value; }
+        public string MailId1 { get => this.MailId; set => this.MailId = value; }
+        public string Password1 { get => this.Password; set => this.Password = value; }
+
+
+        public User()
+        {
+        }
+
+        public User(string username, string mailId, string password)
+        {
+            this.Username = username;
+            this.MailId = mailId;
+            this.Password = password;
+        }
+
+        //constructor
+
+
+
+
+        
+        public override string ToString()
+        {
+            return String.Format("Username: {0}\nMailId: {1}\n Password: {2}" , Username, MailId, Password);
+        }
+        public override bool Equals(object obj)
+        {
+            User obj_user = (User)obj;
+            return this.Username.Equals(obj_user.Username);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+    }
+}
